b801050132ffc18f44fb0e05e868727555a33545
Jacob Turner <jeturner44@outlook.com>
2022-08-08 16:33:36 +1000

    fix: merge latest code from the development branch

--- __test__/integrationTests.test.ts
+++ __test__/integrationTests.test.ts
@@ -37,6 +37,39 @@ afterEach(async () => {
     server.close();
 });
 
+// Middleware tests
+// ----------------------------------------------------------------------------
+
+describe("CORS", () => {
+    it("should implement CORS", async () => {
+        const response = await axios.get("/");
+        expect(response.headers["access-control-allow-origin"]).toEqual("*");
+    });
+});
+
+describe("Rate limit", () => {
+    beforeAll(() => {
+        // Change operating environment just for this test as rate limiter is disabled for test environment
+        process.env.NODE_ENV = "development";
+        expect(process.env.NODE_ENV).toEqual("development");
+    });
+
+    afterAll(() => {
+        // Reset operating environment
+        process.env.NODE_ENV = "test";
+        expect(process.env.NODE_ENV).toEqual("test");
+    });
+
+    it("Should allow no more than 2 requests per second", async () => {
+        const response1 = await axios.get("/");
+        const response2 = await axios.get("/");
+        const response3 = await axios.get("/");
+        expect(response1.status).toBe(200);
+        expect(response2.status).toBe(200);
+        expect(response3.status).toBe(429);
+    });
+});
+
 describe("Integration tests for AUTH routes:", () => {
     // Register route handler integration tests
     // -------------------------------------------------------------------------
