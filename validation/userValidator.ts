ab4403c3c8211c23496687aeaeeaf4e6b14b57d9
Claire <claire.bk.seo@gmail.com>
2022-08-12 16:13:26 +1000

    fix: password pattern
    Change strong password pattern

--- validation/userValidator.ts
+++ validation/userValidator.ts
@@ -5,10 +5,8 @@ const validateLogin = {
     password: validator
         .string()
         .trim()
-        .min(4)
-        .max(10)
         // Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special characte
-        // .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/)
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-@$!%*?&])[A-Za-z\d-@$!%*?&]{6,15}$/)
         .required(),
 };
 
