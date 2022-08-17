4a0614f13ad77b7c452c4480a06ed80745bf34f7
Claire <claire.bk.seo@gmail.com>
2022-08-17 12:20:57 +1000

    fix: usernameValidattor
    add regex pattern for username

--- validation/validationDefinition.ts
+++ validation/validationDefinition.ts
@@ -4,4 +4,9 @@ export const emailValidator = yup.string().email().trim().required();
 export const passwordValidator = yup.string().trim()
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-@$!%*?&])[A-Za-z\d-@$!%*?&]{6,15}$/)
     .required();
-export const usernameValidator = yup.string().trim().required();
+export const usernameValidator = yup.string().trim()
+    // allowed : lower case, upper case, number, ".", "-", "_" 
+    // start and end with alphanumeric characters
+    // ".", "-", "_" do not appear consecutively
+    .matches(/^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){4,16}[a-zA-Z0-9]$/) // ex) abc_d-e.f3
+    .required();
