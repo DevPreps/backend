90020e8d63afccee6def3fbc16dbb6ec1dce98d1
Jacob Turner <jeturner44@outlook.com>
2022-08-18 14:41:52 +1000

    feat: add updatePost custom method to postModel object

--- models/__test__/db.test.ts
+++ models/__test__/db.test.ts
@@ -23,6 +23,7 @@ describe("Unit Tests for Database Object:", () => {
         expect(db.post.count).toBeDefined();
         expect(db.post.createPost).toBeDefined();
         expect(db.post.getPostById).toBeDefined();
+        expect(db.post.updatePost).toBeDefined();
     });
 
     test("contains tag object with default methods", () => {
