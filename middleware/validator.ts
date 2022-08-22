a1d11137b044d123ada30b61307771ff9ff20041
Jacob Turner <jeturner44@outlook.com>
2022-08-22 13:29:13 +1000

    merge: merged latest development updates

--- middleware/validator.ts
+++ middleware/validator.ts
@@ -11,4 +11,3 @@ export const validate = (schema: AnyObjectSchema) => {
         }
     };
 };
-
--- validation/validationDefinition.ts
+++ validation/validationDefinition.ts
@@ -1,11 +1,17 @@
 import * as yup from "yup";
 
 export const emailValidator = yup.string().email().trim().required();
-export const passwordValidator = yup.string().trim()
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-@$!%*?&])[A-Za-z\d-@$!%*?&]{6,15}$/)
+export const passwordValidator = yup
+    .string()
+    .trim()
+    .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[-@$!%*?&])[A-Za-z\d-@$!%*?&]{6,15}$/
+    )
     .required();
-export const usernameValidator = yup.string().trim()
-    // allowed : lower case, upper case, number, ".", "-", "_" 
+export const usernameValidator = yup
+    .string()
+    .trim()
+    // allowed : lower case, upper case, number, ".", "-", "_"
     // start and end with alphanumeric characters
     // ".", "-", "_" do not appear consecutively
     .matches(/^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){4,16}[a-zA-Z0-9]$/) // ex) abc_d-e.f3
