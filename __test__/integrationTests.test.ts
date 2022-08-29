ca4947cde929d14b945154aa45105ac962fa8c20
Jacob Turner <jeturner44@outlook.com>
2022-08-29 14:19:52 +1000

    refactor: extract setup code for integration tests to a separate file for reusability

--- __test__/integrationTests.test.ts
+++ __test__/integrationTests.test.ts
@@ -1,47 +1,17 @@
-import app from "../app";
+import "./testSetup"
 import db from "../models/db";
 import axios from "axios";
 import { prisma } from "../models/prisma";
 import { faker } from "@faker-js/faker";
 
 // Import TypeScript types
-import { Express } from "express"; // Types for Express
-import { PrismaSessionStore } from "@quixo3/prisma-session-store";
-import { Server } from "http";
+
 import { AxiosResponse } from "axios";
 import { RegistrationData, UserWithoutPassword } from "../models/userModel";
 import { PostData, PostWithRelations } from "../models/postModel";
 import { Post, Status, Category } from ".prisma/client";
 
-jest.mock("../models/prisma");
 
-axios.defaults.baseURL = process.env.TEST_APP_URL || "http://localhost:9999";
-axios.defaults.headers.post["Content-Type"] = "application/json";
-axios.defaults.validateStatus = (status) => status < 500;
-
-let store: PrismaSessionStore;
-let expressInstance: Express;
-let server: Server;
-
-beforeEach(async () => {
-    // The code below initialises the test application in a way which allows the
-    // database and session store to be shut down manually. This is necessary
-    // to avoid the test application from leaking memory and allows Jest to exit
-    // cleanly.
-    store = new PrismaSessionStore(prisma, {
-        checkPeriod: 60 * 60 * 1000, // 1 hour in milliseconds
-        dbRecordIdIsSessionId: true,
-    });
-    expressInstance = app(store);
-    server = expressInstance.listen(9999);
-});
-
-afterEach(async () => {
-    // Manual shutdown of the test application session store.
-    // The DB connection is closed manually in the prisma mock file.
-    await store.shutdown();
-    server.close();
-});
 
 // Middleware tests
 // ----------------------------------------------------------------------------
